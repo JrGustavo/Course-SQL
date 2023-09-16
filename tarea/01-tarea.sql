d01458b5029939495b94b7ca0f35919143d22266
JrGustavo <gus@soypetal.com>
2023-09-16 11:41:23 -0500

    Update section 6

--- tarea/01-tarea.sql
+++ tarea/01-tarea.sql
@@ -1,28 +1,48 @@
 
 
 -- 1. Ver todos los registros
-
-SELECT *
-FROM users;
-
+SELECT
+	*
+FROM
+	users;
 
 -- 2. Ver el registro cuyo id sea igual a 10
+SELECT
+	*
+FROM
+	users
+WHERE
+	id = 10;
 
-SELECT * FROM users where id = 10;
 -- 3. Quiero todos los registros que cuyo primer nombre sea Jim (engañosa)
-
-select * from users where name like 'Jim %'
+SELECT
+	*
+FROM
+	users
+WHERE
+	name LIKE 'Jim %';
 
 -- 4. Todos los registros cuyo segundo nombre es Alexander
-
-select * from users where name like '% Alexander'
+SELECT
+	*
+FROM
+	users
+WHERE
+	name LIKE '% Alexander';
 
 -- 5. Cambiar el nombre del registro con id = 1, por tu nombre Ej:'Fernando Herrera'
+UPDATE
+	users
+SET
+	NAME = 'Fernando Herrera'
+WHERE
+	id = 1;
 
-update users set NAME = 'Gustavo Alvarado' where id = 1;
--- 6. Borrar el último registro de la tabla
+
+-- 6. Borrar el último registro de la tabla 
 SELECT max(id) from users;
+
 DELETE FROM users
-WHERE id = (SELECT max(id) from users);
+WHERE id = ( SELECT max(id) from users );
 
 select * from users;
